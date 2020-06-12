1904e30d9224ae051ba267d7d79371244ae0b235
Wenceslao Negrete <vampay@digitaldealers.mx>
2020-06-11 21:37:36 -0500

    Alter field created_at on Course

--- viewer/models.py
+++ viewer/models.py
@@ -4,7 +4,7 @@ class Course(models.Model):
   title = models.CharField(max_length=255)
   slug = models.CharField(max_length=50)
   price = models.DecimalField(max_digits=7,decimal_places=2)
-  created_at = models.DateTimeField()
+  created_at = models.DateTimeField(auto_now_add=True)
 
 class Video(models.Model):
   title = models.CharField(max_length=255)

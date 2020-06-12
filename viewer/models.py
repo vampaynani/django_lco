434b35677ccfccccc4ccd0c4d19dabc64b1fa697
Wenceslao Negrete <vampay@digitaldealers.mx>
2020-06-11 21:33:01 -0500

    Alter field created_at on Video

--- viewer/models.py
+++ viewer/models.py
@@ -10,5 +10,5 @@ class Video(models.Model):
   title = models.CharField(max_length=255)
   description = models.TextField()
   video_url = models.CharField(max_length=255)
-  created_at = models.DateTimeField()
+  created_at = models.DateTimeField(auto_now_add=True)
   course = models.ForeignKey(Course, on_delete=models.CASCADE)
